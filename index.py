1af075387f8133c30b53c7ccb85e31f5fc3c7e7c
Gabriel Parra <gparrac@correo.udistrital.edu.co>
2021-05-22 11:12:15 -0500

    vFinal1

--- index.py
+++ index.py
@@ -1,88 +1,103 @@
 
-from zipfile import Path
-from flask import Flask, render_template , request, flash , url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect
 import os
-from flask.globals import current_app
-from flask.helpers import send_file, send_from_directory
+from flask.helpers import send_file
 from werkzeug.utils import secure_filename
 from os import remove
 from formato_excel import excelLibro
 
 
-
-
-#iicializar y def archivo principal
+# iicializar y def archivo principal
 #
-app=Flask( __name__)
+app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-app.config['ACTUALIZAR_ARCHIVO']="./archivo/"
-#creando ruta de nav
+app.config['ACTUALIZAR_ARCHIVO'] = "./archivo/"
+
+
+def eliminar(lista):
+    print(f'eliminando!!')
+    folder = './archivo/'
+    contador=True
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+
+        if os.path.isfile(file_path) :  # folder+'plantilla.xlsx'
+            for item in lista:
+                if file_path == item:
+                    contador=False
+            if contador == True:
+                print(f'{file_path}#####')
+                os.unlink(file_path)
+            else:
+                contador=True       
+            
+
+# creando ruta de nav
+
+
 @app.route('/')
 def home():
     return render_template('inicio.html')
 
-@app.route('/login',methods=['POST'])
+
+@app.route('/login', methods=['POST'])
 def login():
-    #obtener info
-    error='Credenciales invalidas'
+    # obtener info
+    error = 'Credenciales invalidas'
     nombreU = request.form['nombreU']
     contrasena = request.form['contrasena']
-    if nombreU == 'admin'and contrasena == 'ser123':
+    if nombreU == 'admin' and contrasena == 'ser123':
         print(f'sddasdds')
-        folder = './archivo/' 
-        for the_file in os.listdir(folder): 
-            file_path = os.path.join(folder, the_file)  
-            if os.path.isfile(file_path) and file_path != folder+'plantilla.xlsx' : 
-                    print(f'{file_path}#####')
-                    os.unlink(file_path) 
 
-        return redirect(url_for('control'))     
-    return render_template("inicio.html",error=error)        
-    
+        return redirect(url_for('control'))
+    return render_template("inicio.html", error=error)
+
+
 @app.route('/control')
 def control():
+    eliminar(lista=["./archivo/plantilla.xlsx"])
     return render_template('panel.html')
 
 
 @app.route('/formato', methods=['POST'])
 def formato():
-        #obtener info
-    error='Credenciales invalidas'  
+    # obtener info
+    error = 'Credenciales invalidas'
     numMan = request.form['numMan']
     file = request.files['file']
-    
 
     if request.method == 'POST' and numMan != '' and file != '':
-        sf=secure_filename(file.filename)
-        
+        sf = secure_filename(file.filename)
+
         print(f'ruta----{file.filename}')
-        ruta = './archivo/'+file.filename 
-        if not os.path.exists(ruta):            
-            file.save(os.path.join(app.config['ACTUALIZAR_ARCHIVO'],sf)) 
+        ruta = './archivo/'+file.filename
+        if not os.path.exists(ruta):
+            file.save(os.path.join(app.config['ACTUALIZAR_ARCHIVO'], sf))
         clase1 = excelLibro(ruta)
-        btnDes=clase1.extraer_datos(int(numMan))
-        if btnDes: 
+        btnDes = clase1.extraer_datos(int(numMan))
+        if btnDes:
             clase1.imprimir()
-            print(f'ENTRAND O=================')
-            path = clase1.guardar_info("./archivo/plantilla.xlsx")
-            path = clase1.crear_pdf(path)
+            print(f'ENTRANDo++')
+            path1 = clase1.guardar_info("./archivo/plantilla.xlsx")
+            path = clase1.crear_pdf(path1)
+            print(f'val222')
+            print(f'{path}/// {ruta}/// {path1}')
+            eliminar(lista=[path, ruta, path1, "./archivo/plantilla.xlsx"])
             return send_file(path, as_attachment=True)
         else:
             print(f'elemento no exite')
             remove(ruta)
-            error='No se encotro el numero de manifiesto'
-            
-                        
-        
-          
-    return render_template("panel.html",error = error)   
-         
+            error = 'No se encotro el numero de manifiesto'
+
+    return render_template("panel.html", error=error)
+
+
 @app.route('/download')
-def downloadFile ():
-    #For windows you need to use drive name [ex: F:/Example.pdf]
+def downloadFile():
+    # For windows you need to use drive name [ex: F:/Example.pdf]
     path = "./archivo/xlsx-to-pdf.xlsx"
     return send_file(path, as_attachment=True)
 
 
-if __name__ == '__main__':
-    app.run(debug=False)
+#if __name__ == '__main__':
+ #   app.run(debug=True)
